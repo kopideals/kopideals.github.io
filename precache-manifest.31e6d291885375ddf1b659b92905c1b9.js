563c8b2b5e12e5f55bbc91e1c27fd5eedc820c8b
Fadhiil Rachman <fadhiilrachman@gmail.com>
2022-03-11 07:26:09 +0700

    Release Kopi Deals landing page
    Version 1.0.0

---
+++ precache-manifest.31e6d291885375ddf1b659b92905c1b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b025e44a0e00d44517e",
+    "url": "/static/css/main.0fcc982a.chunk.css"
+  },
+  {
+    "revision": "5b025e44a0e00d44517e",
+    "url": "/static/js/main.da85a5ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16bfe6f5c3f7d4345c47",
+    "url": "/static/js/2.3c68d087.chunk.js"
+  },
+  {
+    "revision": "6471896055da5823ab76499f8164744f",
+    "url": "/static/media/logo.64718960.svg"
+  },
+  {
+    "revision": "8c7e9e8d6494b99ad137851c94aa32d8",
+    "url": "/static/media/kopi-deals.8c7e9e8d.svg"
+  },
+  {
+    "revision": "3cc5887d3834fbba8d574027b29be8a8",
+    "url": "/index.html"
+  }
+];
